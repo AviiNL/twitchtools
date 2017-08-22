faa327638c136f278b4f82ed3a6176eb32f87e27
Yme-Jan Iedema <yme-jan@iedema.me>
2017-08-22 14:06:59 +0200

    Added missing files

---
+++ main.js
@@ -0,0 +1,84 @@
+"use strict";
+exports.__esModule = true;
+var electron_1 = require("electron");
+var dotenv = require("dotenv");
+var electronOauth2 = require("electron-oauth2");
+exports.mainWindow = null;
+dotenv.config();
+var accessToken;
+electron_1.app.on('ready', function () {
+    var myApiOauth = electronOauth2({
+        clientId: 'fmkkosg3lceokpmzkgv8g8sm76nmy2h',
+        clientSecret: 'ec8iawsje6p35u8g3iyl3rbbygc89p',
+        authorizationUrl: 'https://api.twitch.tv/kraken/oauth2/authorize',
+        tokenUrl: 'https://api.twitch.tv/kraken/oauth2/token',
+        useBasicAuthorizationHeader: false,
+        redirectUri: 'http://localhost/'
+    }, {
+        alwaysOnTop: true,
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: false,
+            devTools: false
+        }
+    });
+    myApiOauth.getAccessToken({
+        scope: [
+            'user_read',
+            'user_blocks_edit',
+            'user_blocks_read',
+            'channel_read',
+            'channel_editor',
+            'channel_commercial',
+            'channel_subscriptions',
+            'channel_check_subscription',
+            'chat_login'
+        ].join(' ')
+    })
+        .then(function (token) {
+        myApiOauth.refreshToken(token.refresh_token)
+            .then(function (newToken) {
+            // use your new token
+            accessToken = JSON.stringify(newToken);
+            exports.mainWindow.loadURL(process.env.HOST + '/token/' + accessToken);
+            exports.mainWindow.once('ready-to-show', function () {
+                exports.mainWindow.show();
+            });
+        })["catch"](function (e) {
+            process.stderr.write(e.message + '\n');
+            electron_1.app.quit();
+        });
+    })["catch"](function (e) {
+        process.stderr.write(e.message + '\n');
+        electron_1.app.quit();
+    });
+    exports.mainWindow = new electron_1.BrowserWindow({ frame: false, width: 1200, height: 800, show: false });
+    exports.mainWindow.setMenu(null);
+    // if (process.env.PACKAGE.toLowerCase() === 'true') {
+    //     mainWindow.loadURL(url.format({
+    //         pathname: path.join(__dirname, 'dist/index.html'),
+    //         protocol: 'file:',
+    //         slashes: true
+    //     }));
+    // } else {
+    //     mainWindow.loadURL(process.env.HOST);
+    //     mainWindow.webContents.openDevTools();
+    // }
+    exports.mainWindow.webContents.openDevTools();
+    exports.mainWindow.on('closed', function () {
+        exports.mainWindow = null;
+    });
+});
+electron_1.app.on('activate', function () {
+    if (exports.mainWindow === null) {
+        // Re-create the window
+        // electron.createWindow();
+        electron_1.app.quit(); // todo: fixme
+    }
+    exports.mainWindow.show();
+});
+electron_1.app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        electron_1.app.quit();
+    }
+});
---
+++ main.ts
@@ -0,0 +1,103 @@
+import {app, BrowserWindow} from 'electron';
+import * as dotenv from 'dotenv';
+import * as electronOauth2 from 'electron-oauth2';
+
+export let mainWindow: Electron.BrowserWindow = null;
+
+dotenv.config();
+
+let accessToken: string;
+
+app.on('ready', () => {
+
+    const myApiOauth = electronOauth2({
+        clientId: 'fmkkosg3lceokpmzkgv8g8sm76nmy2h',
+        clientSecret: 'ec8iawsje6p35u8g3iyl3rbbygc89p',
+        authorizationUrl: 'https://api.twitch.tv/kraken/oauth2/authorize',
+        tokenUrl: 'https://api.twitch.tv/kraken/oauth2/token',
+        useBasicAuthorizationHeader: false,
+        redirectUri: 'http://localhost/'
+    }, {
+        alwaysOnTop: true,
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: false,
+            devTools: false,
+        }
+    });
+
+    myApiOauth.getAccessToken({
+        scope: [
+            'user_read',
+            'user_blocks_edit',
+            'user_blocks_read',
+            'channel_read',
+            'channel_editor',
+            'channel_commercial',
+            'channel_subscriptions',
+            'channel_check_subscription',
+            'chat_login'
+        ].join(' ')
+    })
+        .then(token => {
+            myApiOauth.refreshToken(token.refresh_token)
+                .then(newToken => {
+                    // use your new token
+                    accessToken = JSON.stringify(newToken);
+
+                    mainWindow.loadURL(process.env.HOST + '/token/' + accessToken);
+                    mainWindow.once('ready-to-show', () => {
+                        mainWindow.show();
+                    });
+
+                }).catch(e => {
+                    process.stderr.write(e.message + '\n');
+                    app.quit();
+                }
+            );
+
+        })
+        .catch(e => {
+            process.stderr.write(e.message + '\n');
+            app.quit();
+        });
+
+
+    mainWindow = new BrowserWindow({frame: false, width: 1200, height: 800, show: false});
+    mainWindow.setMenu(null);
+
+    // if (process.env.PACKAGE.toLowerCase() === 'true') {
+    //     mainWindow.loadURL(url.format({
+    //         pathname: path.join(__dirname, 'dist/index.html'),
+    //         protocol: 'file:',
+    //         slashes: true
+    //     }));
+    // } else {
+    //     mainWindow.loadURL(process.env.HOST);
+    //     mainWindow.webContents.openDevTools();
+    // }
+
+    mainWindow.webContents.openDevTools();
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+
+
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        // Re-create the window
+        // electron.createWindow();
+        app.quit(); // todo: fixme
+    }
+
+    mainWindow.show();
+});
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
